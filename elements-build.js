5393ea170e70c24ff9b9f0a91a361c5e45eb84f8
Daniel Rodriguez <drod2793@gmail.com>
2019-02-14 10:47:22 -0700

    Adds build script

---
+++ elements-build.js
@@ -0,0 +1,14 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+(async function build() {
+  const files = [
+    './dist/runtime.js',
+    './dist/polyfills.js',
+    './dist/scripts.js',
+    './dist/main.js'
+  ];
+
+  await fs.ensureDir('elements');
+  await concat(files, 'elements/index.js');
+})();
