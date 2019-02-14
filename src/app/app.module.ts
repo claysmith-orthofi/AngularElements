e521f367660e872bd16bfa8054ed74850cb53dd0
Daniel Rodriguez <drod2793@gmail.com>
2019-02-14 12:40:18 -0700

    Removes app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     TestComponent
   ],
-  exports: [
+  entryComponents: [
     TestComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule {
   constructor(private _injector: Injector) { }
